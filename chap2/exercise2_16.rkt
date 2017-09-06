256baf60c1d9069e6308c049baafcd37f4da8056
liscju <piotr.listkiewicz@gmail.com>
2017-09-06 20:59:44 +0200

    Add solution to exercise 2.16

---
+++ chap2/exercise2_16.rkt
@@ -0,0 +1,6 @@
+#lang sicp
+
+; To give the same result as algrebraic ones operation
+; on fraction should be performed without losing precision,
+; this is possible with using another representation for
+; them.
