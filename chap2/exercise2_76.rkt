a492f3a0820196b43ad705dceebb74c11cee8e28
liscju <piotr.listkiewicz@gmail.com>
2017-12-07 23:10:23 +0100

    Add solution to exercise 2.76

---
+++ chap2/exercise2_76.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+; general operations with explicit dispatch
+; add new type -> changing all methods to handle them
+; add new operation -> add new operation to handle this for all types
+;
+; data oriented programming
+; add new type -> install new (separate) type that has all operations
+; add new operation -> add new operation for each type
+;
+;
+; message passing programming
+; add new type -> create new type with operations
+; add new operation -> add new operation for all types
+
+; Easiest adding new type -> message passing programming
+; Easiest adding new operation -> data oriented programming
