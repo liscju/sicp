5df24e78322654117b8fae76194a3481146d8012
liscju <piotr.listkiewicz@gmail.com>
2018-01-16 21:39:09 +0100

    Add solution to exercise 3.31

---
+++ chap3/exercise3_31.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+
+; if accept-actoin-procedure! would be defined as:
+;
+; (define (accept-action-procedure!  proc)
+;    (set! action-procedures (cons proc action-procedures)))
+;
+; it would not invoke for initial value of circuit
