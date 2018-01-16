8fefafde6e2a687f045812b992db12c62b830f2b
liscju <piotr.listkiewicz@gmail.com>
2018-01-16 20:26:54 +0100

    Add solution to exercise 3.28

---
+++ chap2/exercise3_28.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define or-gate-delay 500)
+
+(define (logical-or x y)
+  (or x y))
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value (logical-or (get-signal a1)
+                                 (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
