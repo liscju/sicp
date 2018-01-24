e03e56ed5d65d71ff9ca830f1ba93798c844dd0c
liscju <piotr.listkiewicz@gmail.com>
2018-01-24 19:13:32 +0100

    Add solution to exercise 3.44

---
+++ chap3/exercise3_44.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+; if there is enough money for all, but what
+; if it is not:
+; Suppose there are two account (10, 10)
+;
+; P1 transfers 10 from first to second
+; P2 transfers 10 from second to first
+; Lets suppose that in moment in time P1 takes 10 from first
+; account, P2 takes 10 from second account
+; In this moment in time accounts are (0, 0)
+; In this moment P3 cannot transfer account at all
