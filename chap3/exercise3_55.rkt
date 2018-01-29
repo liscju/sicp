b474389be82082a2af025ec5df9235b500dd97be
liscju <piotr.listkiewicz@gmail.com>
2018-01-29 20:58:21 +0100

    Add solution to exercise 3.55

---
+++ chap3/exercise3_55.rkt
@@ -0,0 +1,53 @@
+#lang sicp
+(#%require (only racket/stream
+                 stream-cons
+                 stream-first
+                 stream-rest
+                 empty-stream
+                 stream-empty?
+                 stream
+                 for/stream
+                 stream-map
+                 stream-ref
+                 stream-filter
+                 stream-for-each))
+
+(define the-empty-stream empty-stream)
+(define stream-car stream-first)
+(define stream-cdr stream-rest)
+(define stream-null? stream-empty?)
+(define (stream-enumerate-interval start end)
+  (if (> start end)
+      empty-stream
+      (stream-cons start (stream-enumerate-interval (inc start) end))))
+(define (display-stream s) (stream-for-each display s))
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (stream-cons
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define ones (stream-cons 1 ones))
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+(define integers (stream-cons 1 (add-streams ones integers)))
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+; exercise
+
+(define (partial-sums s)
+  (define partial (stream-cons (stream-car s) (add-streams (stream-cdr s) partial)))
+  partial)
+
+(define int-partial (partial-sums integers))
+
+
+
+
+
+
+
+
